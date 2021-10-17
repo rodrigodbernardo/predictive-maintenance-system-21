8c8e5f6a4622fce448faa5d085b9d1aa82145180
rodrigodbernardo <rodrigodouglasba@gmail.com>
2021-10-17 11:51:25 -0300

    commit

--- pibiti/esp-firmware/basico/02-envio-de-dados/02-envio-de-dados-mqtt/mpu-pibiti.ino
+++ pibiti/esp-firmware/basico/02-envio-de-dados/02-envio-de-dados-mqtt/mpu-pibiti.ino
@@ -8,6 +8,7 @@ float mapfloat(long x, long in_min, long in_max, long out_min, long out_max){
   return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
 
+
 void sensor::wakeup() {
   //
   // INICIA A CONFIGURAÇÃO DO SENSOR
