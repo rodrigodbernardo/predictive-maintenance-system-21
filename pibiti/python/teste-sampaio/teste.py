dab5df241a60ad9d525cf12d6fba87e125124e3e
rodrigodbernardo <rodrigodouglasba@gmail.com>
2022-04-08 19:06:07 -0300

    commit

--- pibiti/python/teste-sampaio/teste.py
+++ pibiti/python/teste-sampaio/teste.py
@@ -8,11 +8,11 @@ from paho.mqtt import client as mqtt_client
 
 broker = '127.0.0.1'
 port = 1883
-topic = "python/mqtt"
+topic = "/feeds/entrada"
 # generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-username = 'emqx'
-password = 'public'
+username = 'cliente'
+password = 'cliente'
 
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
